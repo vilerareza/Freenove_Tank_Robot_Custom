4e1739157ef53f8d3c6a46b02166fde9945135dd
Reza Vilera <vilera_reza99@yahoo.com>
2023-08-10 21:37:02 +0700

    integrating vision function to ultrasonic program

--- Code/Server/ultrasonic_with_face.py
+++ Code/Server/ultrasonic_with_face.py
@@ -11,10 +11,6 @@ from Motor import *
 from servo import *
 from Ultrasonic import Ultrasonic
 
-# Initialize ultrasonic module
-ultrasonic = Ultrasonic()
-# define a video capture object
-cam = Picamera2()
 
 async def start_ultrasonic():
     
@@ -57,11 +53,11 @@ async def start_camera(flip = True, res=(640,480), audio_out=None):
 
             if len(rects) != 0:
                 print (f'Face detected: {len(rects)}')
-                # if audio_out is not None:
-                #     # If audio is enabled
-                #     if not audio_out.music.get_busy():
-                #         # If the audio is not playing then play the audio
-                #         audio_out.music.play()
+                if audio_out is not None:
+                    # If audio is enabled
+                    if not audio_out.music.get_busy():
+                        # If the audio is not playing then play the audio
+                        audio_out.music.play()
         
             t2 = time.time()
             print (f'frame_time: {t2-t1}')
@@ -73,10 +69,47 @@ async def start_camera(flip = True, res=(640,480), audio_out=None):
             break
 
 
-async def main():
+async def main(audio_out):
     task_ultrasonic = asyncio.create_task(start_ultrasonic())
-    task_camera = asyncio.create_task(start_camera())
+    task_camera = asyncio.create_task(start_camera(audio_out=audio_out))
     await task_ultrasonic
     await task_camera
 
-asyncio.run(main())
+
+if __name__ == '__main__':
+
+    # Argument handling
+    parser = argparse.ArgumentParser()
+    # parser.add_argument('--pred_path', type = str, default = '../shape_predictor_68_face_landmarks.dat', required = False)
+    parser.add_argument('--audio_enabled', type = bool, default = True, required = False)
+    parser.add_argument('--audio_file_path', type = str, default = 'audio_test.mp3', required = False)
+    parser.add_argument('--audio_vol', type = float, default = 0.7, required = False)
+    args = parser.parse_args()
+    # predictor_path = args.pred_path
+    audio_enabled = args.audio_enabled
+    audio_file_path = args.audio_file_path
+    audio_vol = args.audio_vol
+
+    # Initialize ultrasonic module
+    ultrasonic = Ultrasonic()
+    # define a video capture object
+    cam = Picamera2()
+    # Initialize audio out mixer object
+    if audio_enabled:
+        try:
+            # Initialize pygame mixer
+            audio_mixer.init()
+            # Loading the audio
+            audio_mixer.music.load(audio_file_path)
+            # Setting the volume
+            audio_mixer.music.set_volume(audio_vol)
+            # Play the audio file
+
+        except Exception as e:
+            print (f'Error {e}: Check if the audio file path {audio_file_path} is correct')
+            # Exit the program
+            exit()
+    else:
+        audio_out = None
+
+    asyncio.run(main(audio_out))
