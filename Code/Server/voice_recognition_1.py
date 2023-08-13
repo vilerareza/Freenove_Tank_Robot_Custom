7100b777f52c6ac5c976ccfd9e99fc31f7645654
Reza Vilera <vilera_reza99@yahoo.com>
2023-08-13 22:49:41 +0700

    implementing first voice recognition

--- Code/Server/voice_recognition_1.py
+++ Code/Server/voice_recognition_1.py
@@ -5,16 +5,14 @@ from ctypes import *
 r = sr.Recognizer()
 r.energy_threshold = 20
 
-# ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
-# def py_error_handler(filename, line, function, err, fmt):
-#     # Print nothing
-#     pass
-# c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
-# asound = cdll.LoadLibrary('libasound.so')
-# # Set error handler
-# asound.snd_lib_error_set_handler(c_error_handler)
-
-
+ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+def py_error_handler(filename, line, function, err, fmt):
+    # Print nothing
+    pass
+c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+asound = cdll.LoadLibrary('libasound.so')
+# Set error handler
+asound.snd_lib_error_set_handler(c_error_handler)
 
 
 while True:   
@@ -23,22 +21,14 @@ while True:
          
         # use the microphone as source for input.
         with sr.Microphone() as source_:
-            print ('start listening')
             r.adjust_for_ambient_noise(source_, duration=0.2)
-             
             #listens for the user's input
             audio2 = r.listen(source_)
-            
-            print ('detecting sound')
-
             # Using google to recognize audio
             MyText = r.recognize_google(audio2)
             MyText = MyText.lower()
  
             print("Did you say ",MyText)
              
-    except sr.RequestError as e:
+    except Exception as e:
         print("Could not request results; {0}".format(e))
-         
-    except sr.UnknownValueError:
-        print("unknown error occurred")
