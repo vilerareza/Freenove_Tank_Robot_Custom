923881f0a12e97e1308d10570446b84b9df2ad61
Reza Vilera <vilera_reza99@yahoo.com>
2023-08-13 17:36:26 +0700

    implementing first voice recognition

--- Code/Server/voice_recognition_1.py
+++ Code/Server/voice_recognition_1.py
@@ -4,14 +4,14 @@ from ctypes import *
 # Initialize the recognizer
 r = sr.Recognizer()
 
-ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
-def py_error_handler(filename, line, function, err, fmt):
-    # Print nothing
-    pass
-c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
-asound = cdll.LoadLibrary('libasound.so')
-# Set error handler
-asound.snd_lib_error_set_handler(c_error_handler)
+# ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+# def py_error_handler(filename, line, function, err, fmt):
+#     # Print nothing
+#     pass
+# c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+# asound = cdll.LoadLibrary('libasound.so')
+# # Set error handler
+# asound.snd_lib_error_set_handler(c_error_handler)
 
 
 while True:   
